2a346cf85c2621559e1c22b77d8b2d23bd511f71
mkhader96 <mkhader996@gmail.com>
2022-12-03 16:52:56 +0000

    Feat: Added Detail View

--- snacks/views.py
+++ snacks/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView,ListView
+from django.views.generic import TemplateView,ListView,DetailView
 from .models import Snack
 
 class HomePage(TemplateView):
@@ -7,4 +7,8 @@ class HomePage(TemplateView):
 
 class SnackListView(ListView):
     template_name='snack_list.html'
+    model=Snack
+
+class SnackDetail(DetailView):
+    template_name='snack_detail.html'
     model=Snack
